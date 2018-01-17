e333a54832523f97a1baae9bc6b4c2e4b6e2ecde
Joseph Semlak <semlak@gmail.com>
2018-01-16 19:48:32 -0600

    basic authorization and basic firebase config

---
+++ assets/javascript/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+
+  var config = {
+    apiKey: "AIzaSyB8jEYhxSfyoueE957AFNiHq7mIx5UESvU",
+    authDomain: "project1jerks.firebaseapp.com",
+    databaseURL: "https://project1jerks.firebaseio.com",
+    projectId: "project1jerks",
+    storageBucket: "",
+    messagingSenderId: "804142448908"
+  };
+  firebase.initializeApp(config);
