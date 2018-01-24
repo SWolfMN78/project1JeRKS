c1cfba04616b3d93589dd02c9d3efa86f96d2a5f
Wolfknightx <Wolfknightx@Gmail.com>
2018-01-23 18:30:04 -0600

    updated material

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,7 +1,7 @@
-var apiKey = "2373bec66f0a39f64ce440df9759dc85";
-var apiID = "c45cecad";
+// var apiKey = "2373bec66f0a39f64ce440df9759dc85";
+// var apiID = "c45cecad";
 
-var searchCourse = "";
-var searhDish = "";
+// var searchCourse = "";
+// var searhDish = "";
 
-var callSTring = "http://api.yummly.com/v1/api/recipes?_app_id=" + apiID + "&_app_key=" + apiKey + "&q=onion+soup&allowedCuisine[]=cuisine^cuisine-american"
+// var callSTring = "http://api.yummly.com/v1/api/recipes?_app_id=" + apiID + "&_app_key=" + apiKey + "&q=onion+soup&allowedCuisine[]=cuisine^cuisine-american";
